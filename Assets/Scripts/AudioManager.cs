37016ad6f95d85b2eb9730e8cefd444883ebb9af
Trolldig <39638420+Trolldig@users.noreply.github.com>
2018-09-09 15:48:16 +0200

    Add MainMenu
    Main Menu added. Play starts the first Level. Options and Controls show the Controls + makes Sound adjustable. Quit ends the Game.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour {
+
+    public Slider Volume;
+    public AudioSource mainMenuMusic;
+	
+	// Update is called once per frame
+	void Update () {
+        mainMenuMusic.volume = Volume.value;
+	}
+}
